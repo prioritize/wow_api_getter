02e62d5c630a29ad13cd046807c77889a79673ed
prioritize <jared@prototypic.al>
2024-07-30 00:21:21 -0700

    progress commit

--- src/wow/game_data_request.rs
+++ src/wow/game_data_request.rs
@@ -1,7 +1,17 @@
 use reqwest::Client;
-
+use crate::blizz::utils::generate_region_hostname;
 use crate::blizz::utils::Region;
 
-trait GDRequest<T> {
-    async fn get(client: Client, token: String, region: Region, url: String);
+pub trait GDRequest<T> {
+    async fn get(
+        client: Client,
+        token: String,
+        region: Region,
+        url: String,
+        namespace: String,
+        locale: String,
+        query_args: Vec<(&str, &str)>,
+    ) -> T {
+        let base_url = 
+    }
 }
